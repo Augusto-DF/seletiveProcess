0bc15addf488bffd27415acbe2094d2deffb5c00
Augusto-DF <neoaugustocode@gmail.com>
2023-07-24 15:04:46 -0300

    Update input component

--- src/components/Form/Input/index.jsx
+++ src/components/Form/Input/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { StyledInput } from "./styles";
+import { StyledInput, StyledTextArea } from "./styles";
 
 const Input = ({ name, type, label, onChange }) => {
-  return (
+  return type === "text-area" ? (
+    <StyledTextArea
+      name={name}
+      type={type}
+      placeholder={label}
+      onChange={onChange}
+      rows="15"
+    />
+  ) : (
     <StyledInput
       name={name}
       type={type}
--- src/components/Form/Input/styles.js
+++ src/components/Form/Input/styles.js
@@ -9,3 +9,13 @@ export const StyledInput = styled.input`
   border: 1px solid #c7c3c2;
   border-radius: 16px;
 `;
+
+export const StyledTextArea = styled.textarea`
+  width: 520px;
+  height: 220px;
+  padding: 16px;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+  border: 1px solid #c7c3c2;
+  border-radius: 16px;
+`;
